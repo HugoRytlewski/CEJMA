7d12122da0fc43da64778892d7e4cfb4ce483351
Antoine <morellet.antoine@gmail.com>
2024-04-09 23:12:18 +0200

    add Q5

--- components/question5.vue
+++ components/question5.vue
@@ -19,16 +19,12 @@ useScrollFixed(isOpen);
 
     <div class="p-4">
     <div class="mb-4 bg-neutral-900 text-white rounded-lg p-4">
-        <h2 class="text-lg font-semibold">L'Agence nationale de sécurité des systèmes d'information (ANSSI)</h2>
-        <p>Établie en 2009, l'ANSSI est l'autorité nationale en matière de cybersécurité en France. Elle est chargée de la prévention et de la réaction aux incidents informatiques visant les institutions sensibles, ainsi que de l'organisation d'exercices de gestion de crises au niveau national.</p>
+        <h2 class="text-lg font-semibold">Genèse :</h2>
+        <p>Né de la nécessité de renforcer les compétences en cybersécurité face aux défis numériques croissants, le CampusCyber a été lancé pour répondre à la demande croissante de professionnels qualifiés.</p>
     </div>
     <div class="mb-4 bg-neutral-900 text-white rounded-lg p-4">
-        <h2 class="text-lg font-semibold">Le ministère des Armées :</h2>
-        <p>En plus d'assurer la protection des réseaux essentiels à son action, le ministère des Armées intègre également la dimension du combat numérique au cœur de ses opérations militaires. Il a créé un commandement de cyberdéfense pour consolider ses actions dans ce domaine.</p>
-    </div>
-    <div class="bg-neutral-900 text-white rounded-lg p-4">
-        <h2 class="text-lg font-semibold">Le ministère de l'Intérieur</h2>
-        <p>Chargé de lutter contre toutes les formes de cybercriminalité, ce ministère mobilise les services centraux spécialisés ainsi que les réseaux territoriaux de la police nationale, de la gendarmerie nationale et de la sécurité intérieure. Ces services mènent des enquêtes visant à identifier les auteurs d'actes de cybermalveillance et à les déférer à la justice, tout en contribuant à la prévention et à la sensibilisation du public.</p>
+        <h2 class="text-lg font-semibold">Missions :</h2>
+        <p> Le CampusCyber a pour missions de former les talents de demain, de promouvoir la recherche innovante en cybersécurité et de favoriser le rayonnement international de l'expertise française dans ce domaine.</p>
     </div>
   </div>
 </div>
