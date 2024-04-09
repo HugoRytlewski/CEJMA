7696d35dfcf01c8530fa8a7576c7a3b1c59f3408
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2024-04-09 21:43:15 +0200

    add modal

--- components/test.vue
+++ components/test.vue
@@ -1,4 +1,5 @@
 <script setup>
+  const isOpen = ref(false);
 const {
   titre,
 
@@ -9,9 +10,28 @@ titre: String,
 </script>
 
 <template>
-<div class="h-44 w-96 bg-neutral-700 rounded-xl flex items-center justify-center text-white hover:scale-110 duration-300 cursor-pointer">
+<div @click="isOpen=!isOpen" class="h-44 w-96 bg-emerald-500	rounded-xl flex items-center justify-center p-6 text-white cursor-pointer font-bold ">
     {{titre}}
 
 </div>
+<transition>
+<div v-if="isOpen" class="h-screen w-screen bg-red-500 fixed top-0 left-0">
+<p @click="isOpen=!isOpen">X</p>
+</div>
+</transition>
 
 </template>
+
+<style>
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-    <h1 class="text-3xl text-red-500 font-bold underline">
-      Hello HUgo!
+    <h1 class="text-3xl text-white font-bold ">
+      Cejma
     </h1>
+    <div class="flex flex-col gap-6 mt-6">
+      <test
+      titre="Comment se décline la cyberdiplomatie française au niveau européen et international ?"
+      />
 
+      <test
+      titre="Comment se décline la cyberdiplomatie française au niveau européen et international ?"
+      />
+    </div>
+   
     
   </template>
